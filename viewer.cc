e1ad4068b0f3ae12888c673bfbe4d329715aafe9
Pierre Gueth <pierre.gueth@gmail.com>
2013-03-02 20:43:01 +0100

    switched to half kite and dart

--- viewer.cc
+++ viewer.cc
@@ -5,6 +5,7 @@
 
 static const qreal phi = (1.+sqrt(5))/2.;
 static const qreal alpha = M_PI/5.;
+static const qreal rad_to_deg = 180/M_PI;
 
 Viewer::Viewer(QWidget* parent)
 	: QWidget(parent), scale(100)
@@ -12,10 +13,10 @@ Viewer::Viewer(QWidget* parent)
 }
 
 static const QPen pen(Qt::yellow,.05);
-static const QBrush kite_brush(Qt::red);
-static const QBrush dart_brush(Qt::blue);
+static const QColor kite_color(Qt::red);
+static const QColor dart_color(Qt::blue);
 
-void drawKiteUp(QPainter& painter)
+void drawHalfKite(QPainter& painter, unsigned int order)
 {
 	static const QPointF points[] = {
 		QPointF(0,0),
@@ -23,31 +24,15 @@ void drawKiteUp(QPainter& painter)
 		QPointF(phi,0)
 	};
 
-	painter.setPen(Qt::NoPen);
-	painter.setBrush(kite_brush);
+	painter.setPen(kite_color);
+	painter.setBrush(kite_color);
 	painter.drawConvexPolygon(points,3);
 
 	painter.setPen(pen);
 	painter.drawPolyline(points,3);
 }
 
-void drawKiteDown(QPainter& painter)
-{
-	static const QPointF points[] = {
-		QPointF(0,0),
-		QPointF(phi*cos(alpha),-phi*sin(alpha)),
-		QPointF(phi,0)
-	};
-
-	painter.setPen(Qt::NoPen);
-	painter.setBrush(kite_brush);
-	painter.drawConvexPolygon(points,3);
-
-	painter.setPen(pen);
-	painter.drawPolyline(points,3);
-}
-
-void drawDartUp(QPainter& painter)
+void drawHalfDart(QPainter& painter, unsigned int order)
 {
 	static const QPointF points[] = {
 		QPointF(0,0),
@@ -55,30 +40,31 @@ void drawDartUp(QPainter& painter)
 		QPointF(1,0)
 	};
 
-	painter.setPen(Qt::NoPen);
-	painter.setBrush(dart_brush);
+	painter.setPen(dart_color);
+	painter.setBrush(dart_color);
 	painter.drawConvexPolygon(points,3);
 
 	painter.setPen(pen);
 	painter.drawPolyline(points,3);
 }
 
-void drawDartDown(QPainter& painter)
+void drawKite(QPainter& painter, unsigned int order)
 {
-	static const QPointF points[] = {
-		QPointF(0,0),
-		QPointF(1*cos(3*alpha),-sin(3*alpha)),
-		QPointF(1,0)
-	};
-
-	painter.setPen(Qt::NoPen);
-	painter.setBrush(dart_brush);
-	painter.drawConvexPolygon(points,3);
-
-	painter.setPen(pen);
-	painter.drawPolyline(points,3);
+	painter.save();
+	drawHalfKite(painter,order);
+	painter.scale(1,-1);
+	drawHalfKite(painter,order);
+	painter.restore();
 }
 
+void drawDart(QPainter& painter, unsigned int order)
+{
+	painter.save();
+	drawHalfDart(painter,order);
+	painter.scale(1,-1);
+	drawHalfDart(painter,order);
+	painter.restore();
+}
 
 void Viewer::paintEvent(QPaintEvent* event)
 {
@@ -89,11 +75,15 @@ void Viewer::paintEvent(QPaintEvent* event)
 	painter.translate(width()/2,height()/2);
 	painter.scale(scale,scale);
 
-	drawKiteUp(painter);
-	drawKiteDown(painter);
-	painter.translate(phi,0);
-	drawDartUp(painter);
-	drawDartDown(painter);
+	for (int kk=0; kk<5; kk++)
+	{
+		painter.save();
+		painter.rotate(2*kk*alpha*rad_to_deg);
+		drawKite(painter,1);
+		painter.translate(phi,0);
+		drawDart(painter,1);
+		painter.restore();
+	}
 }
 
 
